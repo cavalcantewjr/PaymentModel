12b3e3eb85cb9b8ccbe34ac0e2c90b90402576e4
Washington Cavalcante <cavalcantewjr@gmail.com>
2020-04-19 22:27:13 -0300

    Definindo as entidades

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public List<Subscription> Subscriptions { get; set }
+
+    }
+}
